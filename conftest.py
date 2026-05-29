57dc012e330d5d24c7acf2a9e4e8b4567d89df81
abdulazizablyazov12-prog <abdulazizablyazov12@gmail.com>
2026-05-29 06:42:44 +0500

    fix: add chrome options for CI and downgrade python version to 3.12

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver_chrome():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+    options = Options()
+    # Обязательные аргументы для стабильной работы Selenium внутри GitHub Actions
+    options.add_argument("--headless=new")  # Запуск в фоновом режиме (стандарт для CI)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")  # Заменяет maximize_window()
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
